5e4933db56703232f9524589546d38b568ac724e
Magesh-KGIT <magesh.saran@kgitservices.com>
2023-10-01 01:03:12 +0530

    2st code

--- build_files.sh
+++ build_files.sh
@@ -7,3 +7,7 @@ echo "======> COLLECTING STATIC FILES <======"
 python3.9 manage.py collectstatic --noinput --clear
 echo "======> STATIC FILES COLLECTED <======"
 
+echo "======> MAKE-MIGRATIONS <======"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+echo "======> MAKE-MIGRATIONS-END <======"
